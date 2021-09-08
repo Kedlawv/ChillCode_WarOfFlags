415dda8a7c36e1ec75b08abb504b093afa05c81a
Kedlawv <waldek.bus@gmail.com>
2021-09-08 11:31:23 +0200

    Test => GameMap.actorMatrix creation test

--- src/test/java/com/chillcode/warOfFlags/GameMapTest.java
+++ src/test/java/com/chillcode/warOfFlags/GameMapTest.java
@@ -1,23 +1,20 @@
 package com.chillcode.warOfFlags;
 
-import org.junit.jupiter.api.Assertions;
+import com.chillcode.warOfFlags.actors.Actor;
+import com.chillcode.warOfFlags.actors.ActorFactory;
+import com.chillcode.warOfFlags.actors.Player;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class GameMapTest {
 
     public static GameMap gameMap;
 
     public static final String testMap =
-            "FFFFFFFFFFFFFFFF" + System.lineSeparator()
-                    + "F..............F" + System.lineSeparator() + "F..............F" + System.lineSeparator()
-                    + "F..PRSPRSPRSP..F" + System.lineSeparator() + "F..S........R..F" + System.lineSeparator()
-                    + "F..R........S..F" + System.lineSeparator() + "F..P..FFFF..P..F" + System.lineSeparator()
-                    + "F..S..FFFF..R..F" + System.lineSeparator() + "F..R..FFFF..S..F" + System.lineSeparator()
-                    + "F..P..FFFF..P..F" + System.lineSeparator() + "F..S........R..F" + System.lineSeparator()
-                    + "F..R........S..F" + System.lineSeparator() + "F..PSRPSRPSRP..F" + System.lineSeparator()
-                    + "F..............F" + System.lineSeparator() + "F..............F" + System.lineSeparator()
-                    + "FFFFFFFFFFFFFFFF";
+            "F..R........S..F" + System.lineSeparator() + "F..P..FFFF..P..F";
 
     @BeforeAll
     public static void initGameMap() {
@@ -25,7 +22,77 @@ class GameMapTest {
     }
 
     @Test
-    void givenAStringMatrixThenCorrectActorMatrix() {
-        Assertions.fail();
+    void givenAStringMatrixAndCorrectTypeInActorMatrixThenTrue() {
+        Actor[][] expected = {
+                {ActorFactory.createFlag(gameMap), null, null, ActorFactory.createPlayer(Player.PlayerTeam.ROCK, gameMap),
+                        null, null, null, null, null, null, null, null,
+                        ActorFactory.createPlayer(Player.PlayerTeam.SCISSORS, gameMap), null, null,
+                        ActorFactory.createFlag(gameMap)
+                }
+                ,
+                {ActorFactory.createFlag(gameMap), null, null, ActorFactory.createPlayer(Player.PlayerTeam.PAPER, gameMap),
+                        null, null,
+                        ActorFactory.createFlag(gameMap), ActorFactory.createFlag(gameMap), ActorFactory.createFlag(gameMap),
+                        ActorFactory.createFlag(gameMap), null, null,
+                        ActorFactory.createPlayer(Player.PlayerTeam.PAPER, gameMap), null, null,
+                        ActorFactory.createFlag(gameMap)
+                }
+        };
+
+        Actor[][] actual = gameMap.getActorMatrix();
+
+        int expectedMismatches = 0;
+        int mismatches = getMismatches(expected, actual);
+
+        assertEquals(expectedMismatches, mismatches);
     }
+
+    @Test
+    void givenAStringMatrixAndIncorrectTypeInActorMatrixThenFalse() {
+        Actor[][] expected = {
+                {null, null, null, ActorFactory.createPlayer(Player.PlayerTeam.PAPER, gameMap),
+                        null, null, null, null, null, null, null, ActorFactory.createFlag(gameMap),
+                        ActorFactory.createPlayer(Player.PlayerTeam.SCISSORS, gameMap), null, null,
+                        ActorFactory.createFlag(gameMap)
+                }
+                ,
+                {ActorFactory.createFlag(gameMap), null, null, ActorFactory.createPlayer(Player.PlayerTeam.PAPER, gameMap),
+                        null, null,
+                        ActorFactory.createFlag(gameMap), ActorFactory.createFlag(gameMap), ActorFactory.createFlag(gameMap),
+                        ActorFactory.createFlag(gameMap), null, null,
+                        ActorFactory.createPlayer(Player.PlayerTeam.PAPER, gameMap), null, null,
+                        ActorFactory.createFlag(gameMap)
+                }
+        };
+
+        Actor[][] actual = gameMap.getActorMatrix();
+        
+        int expectedMismatches = 3;
+        int mismatches = getMismatches(expected, actual);
+
+        assertEquals(expectedMismatches, mismatches);
+    }
+
+    private int getMismatches(Actor[][] expected, Actor[][] actual) {
+        int mismatches = 0;
+
+        for (int i = 0; i < actual.length; i++) {
+            for (int j = 0; j < actual[0].length; j++) {
+                Actor act = actual[i][j];
+                Actor exp = expected[i][j];
+                if (act == null) {
+                    if (exp != null) {
+                        mismatches++;
+                    }
+                } else if (exp == null) {
+                    mismatches++;
+                } else if (act.getClass() != exp.getClass()) {
+                    mismatches++;
+                }
+            }
+        }
+        return mismatches;
+    }
+
+
 }
